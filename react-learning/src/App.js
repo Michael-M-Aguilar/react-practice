3f586e52ee085452c5813dd7e87f3461b1f33241
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-10-05 12:33:30 -0700

    Created a hook to handle loading state. Also have a loading message when doing the fetch.

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
 
   async function fetchMovies() {
+    setIsLoading(true)
     const response = await fetch("https://swapi.dev/api/films")
     const data = await response.json()
       const transformedMovies = data.results.map(movieData => {
@@ -18,6 +21,7 @@ function App() {
         };
       });
     setMovies(transformedMovies);
+    setIsLoading(false)
   }
   return (
     <React.Fragment>
@@ -25,7 +29,9 @@ function App() {
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading....</p>}
       </section>
     </React.Fragment>
   );
