9adc47008ef8cae0d0181322e24a5518fa7263b1
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-10-05 12:58:02 -0700

    Learned how to use the useEffect, and the useCallback react methods. Applied it to the current work.

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import MoviesList from './movie/components/MoviesList';
 import './App.css';
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMovies() {
+ const fetchMovies = useCallback(async () => {
     setIsLoading(true)
     setError(null);
 
@@ -33,7 +33,11 @@ function App() {
     setError(error.message);
    }
    setIsLoading(false)
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMovies();
+  }, [fetchMovies]);
 
   let content = <p>Found no movies</p>;
 
