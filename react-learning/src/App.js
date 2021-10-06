9c32fbf90e1339202a9c21be916d2087d110f5f9
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-10-05 22:50:45 -0700

    Checkpoint. Will continue working on POST tomorrow.

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 
 import MoviesList from './movie/components/MoviesList';
 import './App.css';
+import AddMovie from './movie/components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,7 +14,7 @@ function App() {
     setError(null);
 
    try {
-      const response = await fetch("https://swapi.dev/api/films")
+      const response = await fetch("https://react-http-342f9-default-rtdb.firebaseio.com/movies.json");
       if (!response.ok) {
         throw new Error("Something went wrong here.");
       }
@@ -39,6 +40,12 @@ function App() {
     fetchMovies();
   }, [fetchMovies]);
 
+  function addMovieHandler(movie) {
+    fetch("https://react-http-342f9-default-rtdb.firebaseio.com/movies.json",{
+    method: 'POST'
+    });
+  }
+
   let content = <p>Found no movies</p>;
 
   if(movies.length > 0) {
@@ -53,6 +60,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
