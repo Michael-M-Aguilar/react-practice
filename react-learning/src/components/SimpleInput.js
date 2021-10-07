8d57c60709bb516cea8a96dd61da01d4dba00e1e
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-10-07 14:39:26 -0700

    Cleaned up some unncessary hooks and useRef/useEffect. Logic of the input is still present.

--- react-learning/src/components/SimpleInput.js
+++ react-learning/src/components/SimpleInput.js
@@ -1,50 +1,46 @@
-import {useEffect, useRef, useState} from 'react';
+import {useState} from 'react';
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef()
+  // const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState('')
-  const [enteredNameIsValid, setEnteredNameisValid] = useState(false)
   const [enteredNameTouch, setEnteredNameTouch] = useState(false)
 
-  useEffect(() => {
-    if(enteredNameIsValid) {
-      console.log('Name input is valid')
-    }
-  }, [enteredNameIsValid])
+  // useEffect(() => {
+  //   if(enteredNameIsValid) {
+  //     console.log('Name input is valid')
+  //   }
+  // }, [enteredNameIsValid])
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouch;
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
-  };
+
+  }
 
   const nameInputBlurHandler = event =>{
     setEnteredNameTouch(true);
-
-    if (enteredName.trim() === "") {
-      setEnteredNameisValid(false);
-      return;
-    }
   }
 
   const formSubmissionHandler = event => {
     event.preventDefault();
 
-    setEnteredNameTouch(true)
+    setEnteredNameTouch(true);
 
-    if(enteredName.trim() === ''){
-      setEnteredNameisValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-    setEnteredNameisValid(true);
-    console.log(enteredName)
+
+    console.log(enteredName);
 
     // Logic for using Ref
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue)
+    // const enteredValue = nameInputRef.current.value;
+    // console.log(enteredValue)
 
-    setEnteredName('')
+    setEnteredName('');
+    setEnteredNameTouch(false);
   };
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouch;
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
@@ -56,7 +52,7 @@ const SimpleInput = (props) => {
         <label htmlFor="name">Your Name</label>
         <input
           // Assigned nameInputRef using the useRef() method.
-          ref={nameInputRef}
+          // ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
