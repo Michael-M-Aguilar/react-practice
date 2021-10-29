740f3b1efa284714f0d7d04831c790859a0960ed
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-10-29 00:24:25 -0700

    Have the form, have buttons disappear when proceeding in the modal. Next is reading form values to confirm user data is present.

---
+++ react-learning/src/components/Cart/Checkout.js
@@ -0,0 +1,35 @@
+import classes from './Checkout.module.css'
+
+const Checkout = props => {
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={confirmHandler}>
+      <div classname={classes.control}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name"></input>
+      </div>
+      <div classname={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street"></input>
+      </div>
+      <div classname={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal"></input>
+      </div>
+      <div classname={classes.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city"></input>
+      </div>
+      <button type="button" onClick={props.onCancel}>
+        Cancel
+      </button>
+      <button>Confirm</button>
+    </form>
+  );
+}
+
+export default Checkout
