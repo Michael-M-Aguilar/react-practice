97a3b055159fa1a3007e23b33c1ee6b205b72877
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-10-29 23:35:59 -0700

    Creating logic for form validity. Using useRef and useState, as well as helper functions to help validate the form. Not complete yet.

--- react-learning/src/components/Cart/Checkout.js
+++ react-learning/src/components/Cart/Checkout.js
@@ -1,28 +1,58 @@
+import {useRef} from 'react';
+
 import classes from "./Checkout.module.css"
+
+// Helper functions to validate form
+const isEmpty = value => value.trim() === '';
+const isNotFive = value => value.trim() !== 5;
 
 const Checkout = props => {
 
+  const nameInputRef = useRef()
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredPostalIsValid = isNotFive(enteredPostal);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredPostalIsValid &&
+      enteredCityIsValid;
+
+    if (!formIsValid) {
+
+    }
   }
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name"></input>
+        <input type="text" id="name" ref={nameInputRef}></input>
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street"></input>
+        <input type="text" id="street" ref={streetInputRef}></input>
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal"></input>
+        <input type="text" id="postal" ref={postalInputRef}></input>
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city"></input>
+        <input type="text" id="city" ref={cityInputRef}></input>
       </div>
       <div className={classes.action}>
         <button type="button" onClick={props.onCancel}>
