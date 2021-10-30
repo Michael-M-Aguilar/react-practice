339442adc0f12a7b91069eb0689fc9d14fc8e02d
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-10-29 23:26:49 -0700

    Corrected the classname to className for the divs. Also added divs around the buttons.

--- react-learning/src/components/Cart/Checkout.js
+++ react-learning/src/components/Cart/Checkout.js
@@ -1,4 +1,4 @@
-import classes from './Checkout.module.css'
+import classes from "./Checkout.module.css"
 
 const Checkout = props => {
 
@@ -7,27 +7,29 @@ const Checkout = props => {
   }
 
   return (
-    <form onSubmit={confirmHandler}>
-      <div classname={classes.control}>
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name"></input>
       </div>
-      <div classname={classes.control}>
+      <div className={classes.control}>
         <label htmlFor="street">Street</label>
         <input type="text" id="street"></input>
       </div>
-      <div classname={classes.control}>
+      <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
         <input type="text" id="postal"></input>
       </div>
-      <div classname={classes.control}>
+      <div className={classes.control}>
         <label htmlFor="city">City</label>
         <input type="text" id="city"></input>
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.action}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 }
