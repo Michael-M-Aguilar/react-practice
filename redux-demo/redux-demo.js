172aff66d0b541207230a8e5eb03643c25189ee1
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-11-05 10:37:13 -0700

    Have a bug of my commits. Recomitting.

--- redux-demo/redux-demo.js
+++ redux-demo/redux-demo.js
@@ -1,9 +1,17 @@
 const redux = require("redux");
 
 const counterReducer = (state = {counter: 0}, action) => {
+  if (action.type === 'increment'){
   return {
     counter: state.counter + 1
   };
+}
+  if(action.type === 'decrement') {
+    return {
+      coutner: state.counter - 1
+    }
+  }
+return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -18,3 +26,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber)
 
 store.dispatch({type: 'increment'})
+store.dispatch({ type: "decrement" });
