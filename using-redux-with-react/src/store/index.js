7009958af1b52493a6b6565418a2301bd5336a15
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-11-08 11:43:07 -0800

    Learning about createSlice via the @reduxjs/toolkit package. Have most of it set up, just have to figure out how to get store to use this slice now and actions.

--- using-redux-with-react/src/store/index.js
+++ using-redux-with-react/src/store/index.js
@@ -1,38 +1,59 @@
 import {createStore} from 'redux'
+// also has the createReducer,  but slice is more powerful.
+import {createSlice} from '@reduxjs/toolkit'
+export const INCREMENT = "increment";
 
 const initialState = {counter: 0, showCounter: true};
+createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter
+    },
+  },
+});
 
-const counterReducer = (state = initialState, action) => {
-  if  (action.type === 'increment') {
-    return {
-     counter: state.counter + 1,
-     showCounter: state.showCounter
-    }
-  }
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === INCREMENT) {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === 'increaseby5') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
+//   if (action.type === 'increaseby5') {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
+//   if (action.type === 'decrement') {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
 
-  return state
-}
+//   return state
+// }
 
 
 
