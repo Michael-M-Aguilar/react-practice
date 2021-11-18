31143d5685b509f0856cee4cc8248d5403d68804
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-11-18 14:33:01 -0800

    Creating some dispatches for status's. Also created a notification.js and module.css file for the props.status.

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -1,16 +1,24 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import {uiActions} from './store/ui-slice'
 
 function App() {
+  const dispatch = useDispatch()
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
 
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data.'
+      }));
+
       const response = await fetch (
         'https://react-http-342f9-default-rtdb.firebaseio.com/.json',
         {
@@ -20,10 +28,23 @@ function App() {
       );
 
       if (!response.ok) {
-        throw new Error('Sending cart data failed. Please try again.')
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error!",
+            message: "Sending cart data Failed!",
+          })
+        );
       }
-        const responseData = await response.json()
-      };
+
+       dispatch(
+         uiActions.showNotification({
+           status: "Success",
+           title: "Success",
+           message: "Sent Cart Data Succesfully.",
+         })
+       );
+    };
   }, [cart]);
 
   return (
