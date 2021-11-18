7720b4101737c27c78417317358b96fcd4961805
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-11-17 16:38:34 -0800

    Have some basic logic to start connecting this file to a firebase database. In particular if an item exists.

--- advanced-redux/src/components/Shop/ProductItem.js
+++ advanced-redux/src/components/Shop/ProductItem.js
@@ -1,22 +1,35 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector} from 'react-redux'
 
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    dispatch(
-      cartActions.addItemToCart({
-        id,
-        title,
-        price,
-      })
-    );
+    const newTotalQuantity = cart.totalQuanitity + 1;
+
+    const updatedItems = cart.items.slice();
+    const existingItem = updatedItems.find((item)=> item.id === id);
+    if (existingItem) {
+      const updatedItem = { ...existingItem};
+      updatedItem.quantity++;
+      updatedItem.price = updatedItem.price + price;
+      const exisitingItemIndex = updatedItems.findIndex(
+        item => item.id === id
+      );
+    }
+    // dispatch(
+    //   cartActions.addItemToCart({
+    //     id,
+    //     title,
+    //     price,
+    //   })
+    // );
   };
 
   return (
